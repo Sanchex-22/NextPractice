403baae66237d2800e3b82854bfa38d1920c3fee
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2023-10-24 08:23:26 -0500

    Fix layout y navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,16 +2,19 @@
 import Link from "next/link"
 export default function Navbar(){
     return (
-    <nav className="flex justify-between w-full h-16 text-white p-5 border-b border-violet-800 fixed z-0 bg-hinted-grey-3 bg-opacity-90">
+      <nav className="flex justify-between w-full h-16 text-white p-5 border-b border-violet-800 fixed z-0 bg-hinted-grey-3 bg-opacity-90">
         <ul>
-            <li className="font-bold">NextJS</li>
+          <li className="font-bold">NextJS</li>
         </ul>
 
         <ul className="flex">
-            <li className="mr-5 font-bold"><Link href={'/home'}>Home</Link></li>
-            <li className="mr-5 font-bold"><Link href={'/about'}>About</Link></li>
+          <li className="mr-5 font-bold">
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li className="mr-5 font-bold">
+            <Link href={"/about"}>About</Link>
+          </li>
         </ul>
-
-    </nav>
+      </nav>
     )
 }
